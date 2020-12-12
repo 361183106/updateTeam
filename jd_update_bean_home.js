974c5fc134e2a12d0b62acf96e9e56d93b208530
shylocks <shylocksyang@gmail.com>
2020-12-13 00:26:03 +0800

    update by shylocks vps

--- jd_update_bean_home.js
+++ jd_update_bean_home.js
@@ -99,8 +99,7 @@ function getUserInfo() {
               console.log(`未获取到助力码，去开团`)
               await hitGroup()
             } else {
-              console.log(data)
-              console.log(`${data['data']['beanCountProgress']['progressNotYet']}`)
+              console.log(`${data['data']['beanCountProgress']['progressAlready']}`)
               if (data['data']['beanCountProgress']['progressNotYet']===0){
                 $.flag = true
                 console.log(`账号${$.UserName}已满员，跳过`)
