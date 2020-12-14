71ec7194034312ae1b2fdc653457b06f2e1d9e6a
shylocks <shylocksyang@gmail.com>
2020-12-14 16:21:04 +0800

    update by shylocks vps

--- jd_update_bean_home.sh
+++ jd_update_bean_home.sh
@@ -5,5 +5,5 @@ git config --global user.name shylocks
 git add .
 git commit -m "update by shylocks vps" -a || echo "Nothing to update"
 
-git push https://shylocks:$GITHUBTOEKN@github.com/shylocks/updateTeam.git HEAD:main --follow-tags '--force'
+git push https://shylocks:$GITHUBTOKEN@github.com/shylocks/updateTeam.git HEAD:main --follow-tags '--force'
 git push https://shylocks:$GITLEETOKEN@gitee.com/shylocks/updateTeam.git HEAD:main --follow-tags '--force'
