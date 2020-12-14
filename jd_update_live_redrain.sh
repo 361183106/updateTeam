f377a0a8978149aeb284baa1f280213ff31ab2d5
shylocks <shylocksyang@gmail.com>
2020-12-14 16:23:03 +0800

    update by shylocks vps

---
+++ jd_update_live_redrain.sh
@@ -0,0 +1,9 @@
+cd /home/shylocks/Projects/updateTeam
+node jd_update_live_redrain.js
+git config --global user.email shylocksyang@gmail.com
+git config --global user.name shylocks
+git add .
+git commit -m "update by shylocks vps" -a || echo "Nothing to update"
+
+git push https://shylocks:$GITHUBTOKEN@github.com/shylocks/updateTeam.git HEAD:main --follow-tags '--force'
+git push https://shylocks:$GITLEETOKEN@gitee.com/shylocks/updateTeam.git HEAD:main --follow-tags '--force'
