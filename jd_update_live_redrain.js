b6c5d798b22d7d7261469ce7f937f256df27da6b
shylocks <shylocksyang@gmail.com>
2020-12-14 15:14:03 +0800

    update by shylocks vps

--- jd_update_live_redrain.js
+++ jd_update_live_redrain.js
@@ -52,6 +52,8 @@ function getRedRain(body) {
               $.activityId = url.substr(url.indexOf("id=") + 3)
               $.startTime = act.startTime
               $.endTime = act.endTime
+              console.log(`下一场红包雨开始时间：${new Date(act.startTime)}`)
+              console.log(`下一场红包雨结束时间：${new Date(act.endTime)}`)
             } else {
               console.log(`暂无红包雨`)
             }
