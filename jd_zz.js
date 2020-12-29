30e4be3766b9c42e6ec0091e78291b62c43303ee
shylocks <shylocksyang@gmail.com>
2020-12-29 10:37:04 +0800

    update by shylocks vps

--- jd_zz.js
+++ jd_zz.js
@@ -47,10 +47,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         continue
       }
       await jdWish()
-      await writeFile()
-      return
     }
-
   }
   await writeFile()
 })()
@@ -71,7 +68,6 @@ async function jdWish() {
     if ($.hasOpen) await getUserTuanInfo()
   }
   if ($.tuan) $.tuanList.push($.tuan)
-  console.log($.tuanList)
 
   $.tuan = null
   $.hasOpen = false
@@ -81,7 +77,6 @@ async function jdWish() {
     if ($.hasOpen) await getUserTuanInfo("NINE_BOX")
   }
   if ($.tuan) $.tuanList.push($.tuan)
-  console.log($.tuanList)
 }
 async function writeFile() {
   if(!$.tuanList) return
